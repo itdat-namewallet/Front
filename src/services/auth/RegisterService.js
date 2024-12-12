b2583e0e63ad684568e4adc1e7b247f595f9bb44
nicdkim <zzsehdrb@naver.com>
2024-12-12 16:01:20 +0900

    vercel json

--- src/services/auth/RegisterService.js
+++ src/services/auth/RegisterService.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const BASE_URL = process.env.REACT_APP_API_BASE_URL
 
+
 export const registerUser = async (userData) => {
     try {
         const response = await axios.post(`${BASE_URL}/api/auth/register`, userData, {
