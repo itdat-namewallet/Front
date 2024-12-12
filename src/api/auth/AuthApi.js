023e6326cda1808f92da7ea25e99a1a124a2e28c
nicdkim <zzsehdrb@naver.com>
2024-12-12 17:41:26 +0900

    register

--- src/api/auth/AuthApi.js
+++ src/api/auth/AuthApi.js
@@ -8,7 +8,16 @@ const axiosInstance = axios.create({
     }
 });
 
+// 회원가입 API 호출
 export const registerUser = async (userData) => {
     const response = await axiosInstance.post("/api/auth/register", userData);
     return response.data;
+};
+
+// 유효성 검사 API 호출
+export const checkAvailability = async (type, value) => {
+    const response = await axiosInstance.get(`/api/auth/check-availability`, {
+        params: { type, value }
+    });
+    return response.data.available; // 서버에서 반환한 boolean 값이 됨.
 };
