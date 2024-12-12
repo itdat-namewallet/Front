d71213329a2c97cf143230dfc40c3a1cf4cc42e4
nicdkim <zzsehdrb@naver.com>
2024-12-12 14:53:37 +0900

    register

---
+++ src/components/common/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import '../../assets/css/common/header.css';
+
+export default function Header() {
+    return (
+        <header className="header">
+            <h1>ItDat</h1>
+            <nav>
+                <Link to="/" className="nav-link">Home</Link>
+                <Link to="/register" className="nav-link">회원가입</Link>
+            </nav>
+        </header>
+    );
+}
