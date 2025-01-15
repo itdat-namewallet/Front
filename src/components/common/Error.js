fcfd7af6ff9be091955e2359152b21b181ae21d1
kkandda <diobobo@naver.com>
2025-01-15 11:36:14 +0900

    에러 페이지 구현

--- src/components/common/Error.js
+++ src/components/common/Error.js
@@ -1,12 +1,19 @@
-
+import { Link } from "react-router-dom";
+import "../../assets/css/common/error.css"
 
 
 const ErrorPage = () => {
 
     return (
         <>
-            <h1>404 Error</h1>
-            <h1>페이지를 찾을 수 없습니다. </h1>
+            <div className="error-page-main-container">
+                <h1>ITDAT</h1>
+                <br/>
+                <h1>404 Error</h1>
+                <h1>유효하지 않은 도메인 주소입니다.</h1>
+                <br/>
+                <Link to="/">소개 페이지로 이동</Link>
+            </div>
         </>
     )
 }
